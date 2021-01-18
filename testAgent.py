c6e8d3dc501f0472ad10765f692cf3aa2c731771
edwardyang12 <edwardyang016@gmail.com>
2021-01-18 13:37:19 -0800

    test agent

--- testAgent.py
+++ testAgent.py
@@ -5,14 +5,32 @@ import Golf
 
 env = gym.make('Golf-v0')
 
+
+class Agent:
+    def __init__(self):
+        self.network = None
+        # idk what you want to initialize
+        # not allowed to store position or any data about the agent here
+        # can store something like your network or something
+
+    # takes observations as input (position, height, wind)
+    # outputs velocity, club number (see golf_env.py for more details)
+    def step(self, observations):
+        position = observations[0]
+        height = observations[1]
+        wind = observations[2]
+
+        return [0,0]
+
+urAgent = Agent()
 total_reward = 0
 for i in range(5):
     episode_reward = 0
-    env.reset()
+    observation = env.reset()
     for t in range(10):
 
         # agent should determine action based on observation
-        action = env.action_space.sample()
+        action = urAgent.step(observation)
         observation, reward, done, info = env.step(action)
         print("Observations: {} ".format(observation))
         print("Reward: {} ".format(reward))
