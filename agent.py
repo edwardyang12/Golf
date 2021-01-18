29ba3e711e77a5709a9dbf108ca2cecde5b94201
edwardyang12 <edwardyang016@gmail.com>
2021-01-18 15:43:56 -0800

    OKAY STONE

--- agent.py
+++ agent.py
@@ -1,6 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential, layers
+
+# for testing purposes
 class Agent:
-    def __init__(self):
-        self.network = None
+    def __init__(self, model):
+        self.network = self.load_model(model)
         # idk what you want to initialize
         # not allowed to store position or any data about the agent here
         # can store something like your network or something
@@ -8,8 +12,31 @@ class Agent:
     # takes observations as input (position, height, wind)
     # outputs velocity, club number (see golf_env.py for more details)
     def step(self, observations):
-        position = observations[0]
-        height = observations[1]
-        wind = observations[2]
+        
+        position = observations[0][0]
+        height = observations[0][1]
+        wind = observations[0][2]
 
-        return [0,0]
+        output = self.network.predict([observations])[0]
+        return output
+
+    def load_model(self, model):
+        network = create_model()
+        network.load_weights('./checkpoints/tester')
+        return network
+
+def create_model():
+    model= Sequential()
+    model.add(layers.Dense(128, input_shape=(3,)))
+    model.add(layers.Dense(64))
+    model.add(layers.Dense(2))
+    model.compile(optimizer='sgd', loss='mse')
+    return model
+
+
+
+if __name__ == "__main__":
+##    model = create_model()
+##    model.save_weights('./checkpoints/tester')
+      agent = Agent('./checkpoints/tester')
+    
--- testAgent.py
+++ testAgent.py
@@ -2,10 +2,11 @@
 import random
 import gym
 import Golf
+import numpy as np
 from agent import Agent
 
 env = gym.make('Golf-v0')
-urAgent = Agent()
+urAgent = Agent('./checkpoints/tester')
 total_reward = 0
 episodes = 5
 
@@ -13,9 +14,11 @@ for i in range(episodes):
     episode_reward = 0
     observation = env.reset()
     for t in range(10):
-
+        observation = np.array(observation)
+        observation = np.expand_dims(observation, axis=0)
         # agent should determine action based on observation
         action = urAgent.step(observation)
+        np.clip(action,env.action_space.low,env.action_space.high)
         observation, reward, done, info = env.step(action)
         print("Observations: {} ".format(observation))
         print("Reward: {} ".format(reward))
